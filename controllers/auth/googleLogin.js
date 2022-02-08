21e8a86dc9d719e3f72930ec1c3604c5d85b64da
Vitalii Pikozh <wisart@gmail.com>
2022-02-08 16:15:35 +0200

    fix googleautorization

--- controllers/auth/googleLogin.js
+++ controllers/auth/googleLogin.js
@@ -33,7 +33,7 @@ exports.googleRedirect = async (req, res) => {
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.FRONTEND_URL}/api/v1/auth/google-redirect`,
+      redirect_uri: `${process.env.BACKEND_URL}/api/v1/auth/google-redirect`,
       grant_type: "authorization_code",
       code: code,
     },
