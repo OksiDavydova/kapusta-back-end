cf3b9f6b255ebee49b11e470ace8e62d720cc112
Andrii Konovalov <andy.smokk@gmail.com>
2022-02-13 15:07:55 +0100

    reg

--- middlewares/authValidation.js
+++ middlewares/authValidation.js
@@ -1,19 +1,26 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const authSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  email: Joi.string()
+    .email()
+    .required()
+    .pattern(new RegExp("/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i")),
+  password: Joi.string().pattern(
+    new RegExp(
+      "/(?=.*[0-9])(?=.*[!@#-$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g"
+    )
+  ),
 });
 
- const validateAuth = async (req, res,next) => {
-    try {
-         await authSchema.validateAsync(req.body)
-    } catch (err) {
-        return res.status(400).json({message: `Field ${err.message.replace(/"/g, '')}` })
-    }
-     next();
-};  
-
+const validateAuth = async (req, res, next) => {
+  try {
+    await authSchema.validateAsync(req.body);
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: `Field ${err.message.replace(/"/g, "")}` });
+  }
+  next();
+};
 
 module.exports = validateAuth;
-
--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -1,25 +1,32 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const updateBalanceSchema = Joi.object({
-    email: Joi.string().email().required(),
-    balance: Joi.number().optional(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  email: Joi.string()
+    .email()
+    .required()
+    .pattern(new RegExp("/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i")),
+  balance: Joi.number().optional(),
+  password: Joi.string().pattern(
+    new RegExp(
+      "/(?=.*[0-9])(?=.*[!@#-$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g"
+    )
+  ),
 });
 
 const validateId = async (req, res, next) => {
-    if (!Types.ObjectId.isValid(req.params.id)) {
-       return res.status(400).json({ message: 'Invalid ObjectId' })
-   }
-    next()
+  if (!Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: "Invalid ObjectId" });
+  }
+  next();
 };
 
-const validateUpdateBalance = async (req, res,next) => {
-    try {
-         await updateBalanceSchema.validateAsync(req.body)
-    } catch (err) {
-        return res.status(400).json({ message: err.message})
-    }
-     next();
-};  
+const validateUpdateBalance = async (req, res, next) => {
+  try {
+    await updateBalanceSchema.validateAsync(req.body);
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+  next();
+};
 
-module.exports = {validateUpdateBalance, validateId};
+module.exports = { validateUpdateBalance, validateId };
