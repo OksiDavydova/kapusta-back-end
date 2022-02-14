30ffbc1620f3edb9af27238c1d9aaf496543d202
Vitalii Pikozh <wisart@gmail.com>
2022-02-14 12:17:11 +0200

    add filter last six month

--- controllers/transactions/getBalanceTransactions.js
+++ controllers/transactions/getBalanceTransactions.js
@@ -5,37 +5,37 @@ const getBalanceTransactions = async (req, res) => {
   const { token } = req.user;
   const { email } = req.user;
   console.log(email);
-  // try {
-  const gottenData = await axios({
-    url: `${process.env.BACKEND_URL}/api/v1/transactions/incomesandcosts/`,
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  try {
+    const gottenData = await axios({
+      url: `${process.env.BACKEND_URL}/api/v1/transactions/incomesandcosts/`,
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const incomes = gottenData.data.data.filter((el) => el.income === true)[0]
-    .total;
-  const costs = gottenData.data.data.filter((el) => el.income === false)[0]
-    .total;
+    const incomes = gottenData.data.data.filter((el) => el.income === true)[0]
+      .total;
+    const costs = gottenData.data.data.filter((el) => el.income === false)[0]
+      .total;
 
-  const balance = incomes - costs;
+    const balance = incomes - costs;
 
-  const userBalance = {
-    total: balance,
-    income: incomes,
-    expense: costs,
-  };
+    const userBalance = {
+      total: balance,
+      income: incomes,
+      expense: costs,
+    };
 
-  return res.status(StatusCodes.OK).json({
-    messaage: "ok",
-    code: StatusCodes.OK,
-    data: userBalance,
-  });
-  // } catch (error) {
-  //   res
-  //     .status(StatusCodes.BAD_REQUEST)
-  //     .json({ message: error.message, code: StatusCodes.BAD_REQUEST });
-  // }
+    return res.status(StatusCodes.OK).json({
+      messaage: "ok",
+      code: StatusCodes.OK,
+      data: userBalance,
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: error.message, code: StatusCodes.BAD_REQUEST });
+  }
 };
 module.exports = getBalanceTransactions;
